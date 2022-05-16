64cf2e3b73638276b71a3e74daaa5bb564fb43d6
leovasc5 <leovasc5@hotmail.com>
2022-05-16 14:29:55 -0300

    Committing code advances

--- public/js/index.js
+++ public/js/index.js
@@ -21,6 +21,51 @@ fetch("/usuarios/history", {
             sessionStorage.level4 = json[0].level4;
             sessionStorage.level5 = json[0].level5;
         });
+
+        newJanelaA = document.createElement('div');
+        newJanelaA.setAttribute('class', 'newJanelaA');
+        
+        newJanelaAContainer = document.createElement('div');
+        newJanelaAContainer.setAttribute('class', 'newJanelaAContainer');
+
+        newJanelaB = document.createElement('div');
+        newJanelaB.setAttribute('class', 'newJanelaB');
+        
+        newJanelaBContainer = document.createElement('div');
+        newJanelaBContainer.setAttribute('class', 'newJanelaBContainer');
+
+        newJanelaACanvas = document.createElement('Canvas');
+        newJanelaACanvas.setAttribute('id', 'graficoLinha');
+
+        janela.appendChild(newJanelaA);
+        janela.appendChild(newJanelaB);
+        
+        newJanelaA.appendChild(newJanelaAContainer);
+        newJanelaB.appendChild(newJanelaBContainer);
+
+        newJanelaAContainer.appendChild(newJanelaACanvas);
+
+        const data = {
+            labels: ['Muito Fáceis', 'Fáceis', 'Médianas', 'Díficeis', 'Muito Díficeis'],
+            datasets: [{
+              label: 'Sua Melhor Tentativa!',
+              data: [ sessionStorage.level1, sessionStorage.level2, sessionStorage.level3, sessionStorage.level4, sessionStorage.level5],
+              fill: false,
+              borderColor: 'rgb(75, 192, 192)',
+              tension: 0.1
+            }]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+        };
+
+        const grafico = new Chart(
+            document.getElementById('graficoLinha'),
+            config
+        );
+
     } else {
         console.log("Não há dados sobre o usuário");
 resposta.text().then(texto => {
