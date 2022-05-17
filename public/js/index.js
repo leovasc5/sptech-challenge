648096be7ad76cd98fd571abb738c5513523433a
leovasc5 <leovasc5@hotmail.com>
2022-05-16 22:31:52 -0300

    Updating few changes

--- public/js/index.js
+++ public/js/index.js
@@ -34,21 +34,35 @@ fetch("/usuarios/history", {
         newJanelaBContainer = document.createElement('div');
         newJanelaBContainer.setAttribute('class', 'newJanelaBContainer');
 
+        newJanelaAh2 = document.createElement('div');
+        newJanelaAh2.innerHTML = "Melhor Tentativa";
+
         newJanelaACanvas = document.createElement('Canvas');
         newJanelaACanvas.setAttribute('id', 'graficoLinha');
+
+        newJanelaBNovaTentativa = document.createElement('button');
+        newJanelaBNovaTentativa.setAttribute('onclick', 'start()');
+        newJanelaBNovaTentativa.setAttribute('class', 'startButton');
+        newJanelaBNovaTentativa.textContent = 'Iniciar Nova Tentativa';
+
+        newKpis = document.createElement('div');
+        newKpis.setAttribute('class', 'newKpis');
 
         janela.appendChild(newJanelaA);
         janela.appendChild(newJanelaB);
         
+        newJanelaA.appendChild(newJanelaAh2);
         newJanelaA.appendChild(newJanelaAContainer);
         newJanelaB.appendChild(newJanelaBContainer);
+        newJanelaBContainer.appendChild(newJanelaBNovaTentativa);
+        newJanelaB.appendChild(newKpis);
 
         newJanelaAContainer.appendChild(newJanelaACanvas);
 
         const data = {
             labels: ['Muito Fáceis', 'Fáceis', 'Médianas', 'Díficeis', 'Muito Díficeis'],
             datasets: [{
-              label: 'Sua Melhor Tentativa!',
+              label: 'Sua Melhor Tentativa',
               data: [ sessionStorage.level1, sessionStorage.level2, sessionStorage.level3, sessionStorage.level4, sessionStorage.level5],
               fill: false,
               borderColor: 'rgb(75, 192, 192)',
